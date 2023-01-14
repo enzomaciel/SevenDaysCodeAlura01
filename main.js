11939030e917a66272fcaa2f13f2cf5b26cccc8d
Enzo Melo Maciel <90476167+enzomaciel@users.noreply.github.com>
2023-01-14 11:09:13 -0300

    Desafio 2 concluido

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Desafio dia 1
 let numeroUm = 1;
 let stringUm = '1';
 let numeroTrinta = 30;
@@ -21,4 +22,17 @@ if (numeroDez == stringDez) {
   console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes');
 } else {
   console.log('As variáveis numeroDez e stringDez não tem o mesmo valor');
+}
+// Desafio dia 2
+var nome = prompt("Qual o seu nome");
+var idade = prompt("Quantos anos você tem?");
+var linguagem = prompt("Qual a linguagem de programação você está estudando " + nome);
+alert("Olá " + nome + ", você tem "+idade+" anos e já está aprendendo "+linguagem);
+var questao = prompt("Você gosta de estudar " + linguagem + " ? Responda com o número 1 para SIM ou 2 para NÃO");
+if(questao ==1){
+  alert("Muito bom! Continue estudando e você terá muito sucesso.");
+}else if(questao == 2 ){
+  alert("Ahh que pena... Já tentou aprender outras linguagens?");
+}else{
+  alert("ERRO");
 }
