c162906ee91e2c2d2895403ea4e19107e99b9e42
Enzo Melo Maciel <90476167+enzomaciel@users.noreply.github.com>
2023-01-25 19:37:37 -0300

    Começando a estilizar o desafio 03

--- js/desafio02.js
+++ js/desafio02.js
@@ -1,20 +1,18 @@
-const name = pegarNome();
-const linguagem = (`Qual a linguagem de programação você está estudando ${name}?`);
-alert("Olá " + nome + ", você tem "+idade+" anos e já está aprendendo "+linguagem);
-var questao = ("Você gosta de estudar " + linguagem + " ? Responda com o número 1 para SIM ou 2 para NÃO");
-if(questao ==1){
-  alert("Muito bom! Continue estudando e você terá muito sucesso.");
-}else if(questao == 2 ){
-  alert("Ahh que pena... Já tentou aprender outras linguagens?");
-}else{
-  alert("ERRO");
-}
-
-function pegarNome(){
-  const nome = document.getElementById("nome").value;
+function pegarRegistro(registro){
+  const nome = document.getElementById(registro).value;
   return nome;
 }
-function pegarIdade(){
-  const idade = document.getElementById("idade").value;
-  return idade;
+function iniciar(){
+  const nome = pegarRegistro("nome");
+  const idade = pegarRegistro("idade");
+  const linguagem = pegarRegistro("linguagem");
+  alert("Olá " + nome + ", você tem "+idade+" anos e já está aprendendo "+linguagem);
+  const questao = prompt("Você gosta de estudar " + linguagem + " ? Responda com o número 1 para SIM ou 2 para NÃO");
+  if(questao ==1){
+    alert("Muito bom! Continue estudando e você terá muito sucesso.");
+  }else if(questao == 2 ){
+    alert("Ahh que pena... Já tentou aprender outras linguagens?");
+  }else{
+    alert("ERRO");
+}
 }
